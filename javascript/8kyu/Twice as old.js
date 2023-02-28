0205b80855ab8938fa6f8ccde81fb7343e9c5ae6
rzalyaev <ruslan.zalyaev@gmail.com>
2023-03-01 01:17:33 +0400

    Tests passed

---
+++ javascript/8kyu/Twice as old.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/5b853229cfde412a470000d0
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return (dadYearsOld - sonYearsOld * 2) > 0 ? (dadYearsOld - sonYearsOld * 2)
+    : (sonYearsOld * 2 - dadYearsOld);
+}
