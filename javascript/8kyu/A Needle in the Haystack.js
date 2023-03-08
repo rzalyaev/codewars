2fe4541b79e899c085ccacee9ef8c51426e32c6d
rzalyaev <ruslan.zalyaev@gmail.com>
2023-03-08 21:20:21 +0400

    Tests passed

---
+++ javascript/8kyu/A Needle in the Haystack.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
+
+// My variation
+
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf('needle')}`
+}
+
+// Best practice is kinda same
