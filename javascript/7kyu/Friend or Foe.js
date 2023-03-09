c8a13b78f909f8eee4b5669eb815a133f1cded9f
rzalyaev <ruslan.zalyaev@gmail.com>
2023-03-09 23:19:33 +0400

    Tests passed

---
+++ javascript/7kyu/Friend or Foe.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55b42574ff091733d900002f
+
+// My variation
+
+function friend(friends){
+  return friends.filter(e => e.length == 4);
+}
+
+// Best practice is the same
