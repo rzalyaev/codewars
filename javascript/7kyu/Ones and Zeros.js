c736ec5041286432b145562f17f0c42419cd3797
rzalyaev <ruslan.zalyaev@gmail.com>
2023-03-11 20:49:43 +0400

    Tests passed

---
+++ javascript/7kyu/Ones and Zeros.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/578553c3a1b8d5c40300037c
+
+// My variation
+
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(''), 2);
+};
+
+// Best practice is kinda same
